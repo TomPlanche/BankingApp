91a117078c842c6bacdef363470ccd7deba3d806
Tom Planche <58936594+TomPlanche@users.noreply.github.com>
2021-06-01 22:54:23 +0200

    Initial Commit

---
+++ BankingApp/BankingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingAppApp.swift
+//  BankingApp
+//
+//  Created by Tom Planche on 01/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct BankingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BankingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BankingApp
+//
+//  Created by Tom Planche on 01/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
